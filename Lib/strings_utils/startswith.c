aed1aff9e2366775346296ece73d82333c9021dc
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2024-06-15 10:08:32 -0300

    Add files via upload

---
+++ Lib/strings_utils/startswith.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <bool.h>
+
+bool string_startswith(const char* __string, const char* __value)
+{
+    int value_length = strlen(__value);
+    int string_length = strlen(__string);
+    if (value_length == 0){ // the __value is ""
+        return true;
+    }
+
+    if (string_length < value_length){
+        return false;
+    }
+
+    for (int i = 0; i < value_length; i++){
+        char s_char = __string[i];
+        char v_char = __value[i];
+        if (s_char != v_char){
+            return false;
+        }
+    }
+
+    return true;
+}
