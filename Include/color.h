96e5693b1344be4447d1878a6e015fb916463643
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2024-06-15 17:31:54 -0300

    Bug Fix

--- Include/color.h
+++ Include/color.h
@@ -7,17 +7,17 @@ extern "C" {
 
 char* __color(int);
 
+void setcolor(const char*);
+
+#ifdef __cplusplus
+}
+#endif
+
 #define RESET __color(0)
 #define RED __color(1)
 #define GREEN __color(2)
 #define YELLOW __color(3)
 #define BLUE __color(4)
 #define CYAN __color(5)
-
-void setcolor(const char*);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
