f119d2e8aff810895dfb2abfdfdff16b3cd0b046
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2024-06-15 16:53:14 -0300

    Fix Compatibility

--- Include/bool.h
+++ Include/bool.h
@@ -3,10 +3,8 @@ some systems does not come with stdbool.h,
 so this file is necessary
 */
 
-#ifndef _BOOL_H
-#define _BOOL_H
-
 #ifndef __cplusplus
+
 #ifndef bool
 typedef int bool;
 #define bool bool
@@ -24,10 +22,13 @@ typedef int bool;
 #define _STDBOOL_H
 #endif
 
+#ifndef _BOOL_H
+#define _BOOL_H
+#endif
+
 /* Signal that all the definitions are present.  */
 #ifndef __bool_true_false_are_defined
 #define __bool_true_false_are_defined 1
 #endif
-#endif /*__cplusplus*/
 
-#endif /*_BOOL_H*/
+#endif /* __cplusplus */
