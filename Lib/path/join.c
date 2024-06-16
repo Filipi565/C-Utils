3e843525b2bfc6c72bca1cf8a3c4b97875dba012
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2024-06-16 07:24:20 -0300

    Removed Unused Variables and Memory Allocate Bug Fix

--- Lib/path/join.c
+++ Lib/path/join.c
@@ -27,21 +27,17 @@ bool _string_startswith(const char* __string, const char* __value)
 char* _string_reverse(const char* __string)
 {
     int string_length = strlen(__string);
-
-    char temp_result[string_length+1];
+    char* result = (char*)malloc(string_length+1);
 
     int i1 = 0;
     int i2 = string_length-1;
 
     while (i2 >= 0){
-        temp_result[i1] = __string[i2];
+        result[i1] = __string[i2];
         i1++;
         i2--;
     }
-    temp_result[string_length] = '\0'; // the end of the string
-
-    char* result = (char*)malloc(string_length+1);
-    strcpy(result, temp_result);
+    result[string_length] = '\0'; // the end of the string
 
     return result;
 }
